226b134f44c46126c879aa40d4da3e69c49fba10
samzhang0921 <1352152608zyp@gmail.com>
2016-11-18 14:34:06 +0000

    add grunt jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,9 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+    
 
   grunt.initConfig({
     less: {
@@ -14,11 +16,16 @@ module.exports = function(grunt){
       }
     },
     sass: {
-      target: {
-        files: {
-          'public/build/style.css': 'public/css/style.scss'
-        }
-      }
+         build: {
+                options: {
+//                    sourceMap: true,
+//                    outputStyle: 'expanded'
+                },
+                files: {
+                    'public/build/style.css': 'public/css/style.scss'
+                }
+            },
+    
     },
     uglify: {
       build: {
@@ -36,13 +43,28 @@ module.exports = function(grunt){
         dest: 'public/javascripts/build/build.js',
       },
     },
+      
+    jshint: {
+            files: ['Gruntfile.js', 'public/javascripts/**/*.js'],
+            options: {
+                    globals: {
+                    jQuery: true
+                }
+            }
+    },
+      
     watch:{
          sass: {
-                files: ['src/scss/**/*.scss'],
-                tasks: ['sass:build',]
+                files: ['public/css/**/*.scss'],
+                tasks: ['sass:build']
+            },
+        
+        js: {
+                files: ['Gruntfile.js', 'public/javascripts/**/*.js'],
+                tasks: ['jshint', 'concat:build', 'uglify:build']
             },
     }
   });
 
-  grunt.registerTask('default', ['less', 'concat', 'uglify','watch']);
+  grunt.registerTask('default', ['less', 'concat', 'uglify','watch','jshint']);
 };

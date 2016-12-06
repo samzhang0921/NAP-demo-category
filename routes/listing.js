adfe9ad94c51e3122e363263f921fe6539c42492
samzhang0921 <1352152608zyp@gmail.com>
2016-12-06 15:45:14 +0000

    fix

--- routes/listing.js
+++ routes/listing.js
@@ -1,14 +1,16 @@
 var listingpage = require("../controller/listingview");
 var middleware = require('../middleware/middleware');
 
-module.exports = function(app){
-  app.get('/hello/:name', listingpage.hello);
+module.exports = function (app) {
+    app.get('/hello/:name', listingpage.hello);
 
-  app.get('/:language/shop',
-    middleware.setLanguage,
-    middleware.init,
-    middleware.setQuery,
-    middleware.processProducts,
-    listingpage.render
-  );
+    app.get('/:language/shop/:categories',
+        middleware.setLanguage,
+        middleware.init,
+        middleware.setCategories,
+        middleware.setCaregoriesProducts,
+        middleware.setQuery,
+        middleware.processProducts,
+        listingpage.render
+    );
 };

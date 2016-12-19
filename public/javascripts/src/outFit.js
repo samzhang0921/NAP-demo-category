2256c899bc19594b78864db88421e9505223a9a1
samzhang0921 <1352152608zyp@gmail.com>
2016-12-19 10:12:15 +0000

    fix meg

--- public/javascripts/src/outFit.js
+++ public/javascripts/src/outFit.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-(function(){
-    var button = document.querySelectorAll ('.product-image-view');
-    
-     button = [].slice.call(button);
-    
-    for (var i=0; i<= button.length; i++ ){
-        button[i].addEventListener ('onclick', function(e){
-            e.preventDefault;
-        })
-    }
-    
-    console.log(button);
-})();
-=======
 (function () {
     var outFit = document.querySelector('.outfit');
     var product = document.querySelector('.product');
